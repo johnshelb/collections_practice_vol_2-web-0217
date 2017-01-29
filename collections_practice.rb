b06ef34900fff920cf3ee9844f0d20a6a8d18176
johnshelb <jsnd11@gmail.com>
2017-01-29 18:15:59 -0500

    better version

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+=begin
 require 'pry'
 def begins_with_r(array)
   array.all? {|word| word.start_with?('r')}
@@ -53,12 +54,14 @@ def count_elements(array)
   end
 answer
 end
+
 def merge_data(keys,data)
   for i in (0..data.count)
     data[0][data[0].keys[i]][:first_name]=data[0].keys[i]
   end
   data[0].values
 end
+
 def find_cool(cool)
   answer=[]
   for i in (0...cool.count)
@@ -68,11 +71,11 @@ def find_cool(cool)
   end
   answer
 end
+
 def organize_schools(schools)
   new_hash={}
   school_by_city_array=[]
   cities_array=[]
-  #school_name_array=schools.keys
   arrayof_cities_array=schools.values
   for i in (0...arrayof_cities_array.count)
     cities_array<<arrayof_cities_array[i].values[0]
@@ -92,4 +95,69 @@ def organize_schools(schools)
     end
   end
   new_hash
+end
+=end
+
+
+require 'pry'
+def begins_with_r(array)
+  array.all? {|ele| ele.start_with?('r')}
+end
+
+def contain_a(array)
+  array.select {|ele| ele.include?('a')}
+end
+
+def first_wa(array)
+  array.find {|ele| ele.to_s.start_with?('wa')}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|ele| ele.class!=String}
+end
+
+
+def count_elements(array)
+  array.uniq.each do |name_hash1|
+    name_hash1[:count]=0
+    array.each do |name_hash2|
+      if name_hash1[:name]==name_hash2[:name]
+        name_hash1[:count]+=1
+      end
+    end
+  end
+end
+
+def merge_data(keys,data)
+  new_hash={}
+  new_array=[]
+  keys.each do |hash|
+    hash.each do |key,value|
+      data.each do |datahash|
+        datahash.each do |k,v|
+          new_hash=v
+          if k==value
+            new_hash[:first_name]=value
+            new_array<<new_hash
+          end
+        end
+      end
+    end
+  end
+new_array
+end
+
+def find_cool(array)
+  array.select do |hash|
+    hash.values.include?("cool")
+  end
+end
+
+def organize_schools(schools)
+  new_hash={}
+  schools.each do |school_name,location_hash|
+      new_hash[location_hash[:location]]||=[]
+      new_hash[location_hash[:location]]<<school_name
+  end
+new_hash
 end
